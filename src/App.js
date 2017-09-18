2fd1dc4f3247c96465679bb40708d2d4590a6772
William Elliott <wcelliott1991@gmail.com>
2017-09-18 14:59:50 -0400

    updates featured video state on click

--- src/App.js
+++ src/App.js
@@ -12,11 +12,15 @@ class App extends Component {
   constructor(props) {
     super(props);
       this.state = {
-        videos: [], 
+        videos: [],
+        selectedVideo: null, 
       };
 
       YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
-        this.setState({videos});
+        this.setState({
+          videos: videos,
+          selectedVideo: videos[0],
+        });
       });
   }
   render() {
@@ -27,8 +31,10 @@ class App extends Component {
           <h2>WhoTube</h2>
         </div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
