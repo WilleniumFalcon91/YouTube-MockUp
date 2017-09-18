702b17b43e0b1c60449949a71291a9c6dde3976e
William Elliott <wcelliott1991@gmail.com>
2017-09-18 14:02:53 -0400

    calls YouTube api

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './searchBar';
+
+const API_KEY = 'AIzaSyCo_LE6xezdYhgVj9tu5nlPxVpElcU4dgI';
+
+YTSearch({ key: API_KEY, searchTerm: 'surfboards'}, function(data) {
+    console.log(data);
+});
+
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const API_KEY = 'AIzaSyCo_LE6xezdYhgVj9tu5nlPxVpElcU4dgI';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
