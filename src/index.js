557822dbce11e7228df3041a9ef97282736a99ea
William Elliott <wcelliott1991@gmail.com>
2017-09-18 11:44:12 -0400

    creates component files and adds API key

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const API_KEY = 'AIzaSyCo_LE6xezdYhgVj9tu5nlPxVpElcU4dgI';
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
